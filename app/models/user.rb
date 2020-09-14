ed49ad39a3cc48a95a2f7b873f6d79360ac558b9
Annmarie De Silva <annmarie.ds@hotmail.com>
2020-09-14 17:47:37 +1000

    seeds added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :votes
   has_one_attached :photo
 
-  validates :first_name, presence: true, uniqueness: { scope: :last_name }
+  validates :first_name, uniqueness: { scope: :last_name }
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true
 end
