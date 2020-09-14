8902fc41487edb75c440061ce31a97f4097b2414
Sharon Sheah <sharonsheah@gmail.com>
2020-09-14 16:23:14 +1000

    added solutions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :problems, only: [ :index, :new, :create ]  do
+  resources :problems, only: [ :index, :show, :new, :create ]  do
     collection do
       get :leaderboard
+    end
+
+    resources :solutions, only: [ :index, :show, :new, :create ] do
+      collection do
+        get :leaderboard
+      end
     end
   end
 end
