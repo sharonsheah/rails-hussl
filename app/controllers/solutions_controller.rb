8fb7ccffb109cf8b3b1526566460a9bb6429ad3d
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-15 12:25:41 +1000

    new function done

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -7,16 +7,27 @@ class SolutionsController < ApplicationController
     @solution = Solution.find(params[:id])
   end
 
-  # Need to fix error with problem_id and id swapped in url path
 
   def new
     @solution = Solution.new
   end
 
   def create
+    @solution = Solution.new(set_params)
+    @problem = Problem.find(params[:problem_id])
+    @solution.problem = @problem
+    @solution.save!
+
+    redirect_to problem_path(@problem)
   end
 
   # def leaderboard
   #     @solutions = Solution.order("votes").last(5)
   # end
+
+  private
+
+	def set_params
+		params.require(:solution).permit(:title, :description)
+	end
 end
