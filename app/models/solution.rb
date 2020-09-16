69bebc5dc83ce946513d5de80b7e41ba8c04e614
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-16 16:22:11 +1000

    setting up icons

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -4,7 +4,7 @@ class Solution < ApplicationRecord
   has_many :pitches
   has_many :votes, as: :votable
   has_many :collaborations
-  has_many :users, through: :collaborations
+  has_many :collaborators, through: :collaborations, source: :user
 
   validates :title, presence: true, uniqueness: true, length: { in: 6..60 }
   validates :description, presence: true, length: { maximum: 360 }
