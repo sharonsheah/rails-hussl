fabbb07b88b761b2bb1037bec05eb433c443f8cc
Sharon Sheah <sharonsheah@gmail.com>
2020-09-16 16:56:42 +1000

    tried implementing back to prev url button but not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ puts 'Wiping data...'
 Pitch.destroy_all
 Solution.destroy_all
 Problem.destroy_all
+Vote.destroy_all
 User.destroy_all
 
 puts 'Creating 51 users...'
@@ -148,10 +149,9 @@ puts 'Creating pitches...'
 
 # PITCHES
 
-airbnb_pitch = Pitch.create!(title: "Airbnb", description: "Welcome to our pitch. A web platform where users can rent out their space to host travellers to: save money, make money, share culture.", link: "https://www.slideshare.net/ryangum/tinder-pitch-deck", user: airbnb_founder, solution: airbnb_solution)
+airbnb_pitch = Pitch.create!(title: "Airbnb", description: "Welcome to our pitch. A web platform where users can rent out their space to host travellers to: save money, make money, share culture.", link: "https://docs.google.com/presentation/d/1BFFLHUN6acErGNW-huhD6OHMFlXPVmBDdZHFkj46Bg4/preview#slide=id.p", user: airbnb_founder, solution: airbnb_solution)
 
-uber_pitch = Pitch.create!(title: "Uber app", description: "Welcome to our pitch. A web platform where users can rent out their space to host travellers to: save money, make money, share culture.", link: "https://www.slideshare.net/startuphome/uber-pitch-deck-87832684", user: uber_founder, solution: uber_solution)
-
+uber_pitch = Pitch.create!(title: "Uber app", description: "Welcome to our pitch. A web platform where users can rent out their space to host travellers to: save money, make money, share culture.", link: "//www.slideshare.net/slideshow/embed_code/key/jrFtdtaSImlVh1" , user: uber_founder, solution: uber_solution)
 
 puts "Created #{User.all.count} users, #{Problem.all.count} problems, #{Solution.all.count} solutions, #{Pitch.all.count} pitches!"
 
