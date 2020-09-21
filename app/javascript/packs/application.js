13f270cd310e762155813e7ad4861ad1dc75dc3c
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-21 15:19:58 +1000

    all working, linking it to solutions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,8 +29,9 @@ LocalTime.start();
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initChatroomCable } from '../channels/chatroom_channel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initChatroomCable();
 });
