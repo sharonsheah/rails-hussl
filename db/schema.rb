dd626c8e84a330e77cff54948576097e55447792
Raelene Ho <raeyean@gmail.com>
2020-09-21 19:49:09 +1000

    FEAT: created comment form on problem show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_09_21_012340) do
+ActiveRecord::Schema.define(version: 2020_09_21_062908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_012340) do
     t.index ["user_id"], name: "index_collaborations_on_user_id"
   end
 
-<<<<<<< HEAD
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
@@ -65,7 +63,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_012340) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-=======
+  end
+
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "chatroom_id", null: false
@@ -74,7 +73,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_012340) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
->>>>>>> master
   end
 
   create_table "pitches", force: :cascade do |t|
@@ -146,12 +144,9 @@ ActiveRecord::Schema.define(version: 2020_09_21_012340) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "collaborations", "solutions"
   add_foreign_key "collaborations", "users"
-<<<<<<< HEAD
   add_foreign_key "comments", "users"
-=======
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
->>>>>>> master
   add_foreign_key "pitches", "solutions"
   add_foreign_key "pitches", "users"
   add_foreign_key "problems", "users"
