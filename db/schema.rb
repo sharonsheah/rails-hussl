64e4bcf9204673b58ff8f9d26e079f0ea9e3aabf
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-21 10:49:13 +1000

    status added to collab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_064219) do
+ActiveRecord::Schema.define(version: 2020_09_21_004544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_064219) do
     t.bigint "solution_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["solution_id"], name: "index_collaborations_on_solution_id"
     t.index ["user_id"], name: "index_collaborations_on_user_id"
   end
