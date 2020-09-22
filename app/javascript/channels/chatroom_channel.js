926e2c1449965bc51fb50af90cfa08be118b6027
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-22 12:57:06 +1000

    user image fixed

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -7,7 +7,6 @@ const initChatroomCable = () => {
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        console.log(data)
         messagesContainer.insertAdjacentHTML('beforeend', data);
       }
     });
