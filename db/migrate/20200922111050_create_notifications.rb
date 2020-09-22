5f5acaf20dd25b923264567aa2918730a9cde244
Sharon Sheah <sharonsheah@gmail.com>
2020-09-22 21:11:00 +1000

    notification model created

---
+++ db/migrate/20200922111050_create_notifications.rb
@@ -0,0 +1,11 @@
+class CreateNotifications < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notifications do |t|
+      t.string :content
+      t.boolean :status
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
