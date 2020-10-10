0a9decdf4186fc9545042550c8d4409afd1f9b25
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-10-10 20:31:40 +1100

    skip authentication for show and index pages

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,4 +1,6 @@
 class SolutionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  
   def index
     @solutions = Solution.all
     @categories = Problem::CATEGORY
